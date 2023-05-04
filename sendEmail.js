77fcd91efe92bd6d3802461f389c7665f2bbc65c
5Reason <RuizhengWu@outlook.com>
2023-05-04 20:42:03 +0800

    测试修改后的环境变量获取

--- sendEmail.js
+++ sendEmail.js
@@ -19,7 +19,7 @@ console.log("test here:")
 console.log("env:",process.env.SENDER)
 
 const get_env = (key) => {
-    let env_val = process.env.key;
+    let env_val = process.env[key]; // 获取对应参数的环境变量
     if(env_val == undefined){
         return error("对于输入的key:",key,",没有设置环境变量");
     } else {
@@ -32,16 +32,16 @@ let transporter = nodemailer.createTransport({
     port: '465',
     secureConnection: true,
     auth: {
-    //   user: get_env("SENDER"),
-    //   pass: get_env("MAIL_PASS"),
-        user: process.env.SENDER,
-        pass: process.env.MAIL_PASS
+      user: get_env("SENDER"),
+      pass: get_env("MAIL_PASS"),
+        // user: process.env.SENDER,
+        // pass: process.env.MAIL_PASS
     },
 });
 
 const mailOptions = {
-    // from: get_env("SENDER"),
-    from: process.env.SENDER,
+    from: get_env("SENDER"),
+    // from: process.env.SENDER,
     to: 'RuizhengWu@outlook.com',
     subject: '一封测试邮件',
     html: htmlStr
