0177687dc10c48b03689147f0372a96b7a6b4a04
5Reason <RuizhengWu@outlook.com>
2023-05-06 01:07:01 +0800

    测试批量邮件

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // index.js
 // 读取、处理各种配置文件
 
+// 自己的函数
+// 引入 sendEmail 函数
+const sendEmail = require('./path/to/your/file');
+
+// 外部库
 const fs = require('fs');           // node 的文件处理库
 const yaml = require('js-yaml');    // yaml 处理库
 const dayjs = require('dayjs');     // 引入 dayjs 时间处理库
@@ -22,15 +27,13 @@ try{
     const fileContents = fs.readFileSync(birthdayPath, 'utf8');
     const data = yaml.load(fileContents);
     birthdayInfo = [...data.People];
-    console.log("看看 birthday");
-    console.log(birthdayInfo);
 } catch (e) {
     console.log(e);
     console.log("YAML 文件格式错误")
 }
 // 2. 遍历数组，利用 dayjs 计算时间差
 var now = dayjs();
-console.log(now);
+var reminds = []; // 对应发送邮件那块的提醒0，存储所有需要提醒的信息，把这个作为参数传出去
 for(let people of birthdayInfo){
     // 属性
     const birthdate = people.Birthdate;
@@ -39,8 +42,7 @@ for(let people of birthdayInfo){
     const email = people.Email;                 // 选配
     const advancedDay = people.AdvancedDay ?    // 选配
                         people.AdvancedDay : [...defaultAdvance]; 
-
-    console.log(name, birthdate, calendar, email, advancedDay);
+    // console.log(name, birthdate, calendar, email, advancedDay);
     
     // 这里这个逻辑还真得捋一捋哎
     // 0. 时区问题，要 push 之后测试一下
@@ -52,8 +54,6 @@ for(let people of birthdayInfo){
     //      对于 01.01 也应该用 2024.01.01 来计算
     //      对于 12.30 则应该用 2023.12.30 来计算
         
-    // const recordedDate = dayjs(birthdate); // 这里做一些修改
-
     // 将日期字符串转化为 dayjs 对象，允许多种格式的输入 进行容错
     var recordedBirthday = dayjs(birthdate,
                                     ["YYYY-MM-DD",
@@ -61,55 +61,90 @@ for(let people of birthdayInfo){
                                      "YYYY-MM-D",
                                      "YYYY-M-D",
                                      "MM-DD","M-DD","MM-D","M-D"]);
-    var birthday_calculate = recordedBirthday.clone(); // 复制一份，用于计算天数差
-
-    // 如果是农历，需要进行转化 (有点复杂啊烙铁...)
+    let diffInDays; // 距离朋友生日的天数
+    let solar_date;
+    // 1. 农历的情况, calendar == "lunar"
     if(calendar == "lunar"){
         // 先算出此时此刻，在农历算哪一年
         let cur_lunar_year = lunar_trans.solar2lunar(now.year(),now.month()+1,now.date()).lYear;
-        console.log("此时此刻的农历年:", cur_lunar_year);
+        // console.log("此时此刻的农历年:", cur_lunar_year);
         // 用当前的农历年 + 记录的农历月/日 计算出公历日期
         let {cDay, cMonth, cYear} = lunar_trans.lunar2solar(cur_lunar_year, recordedBirthday.month()+1, recordedBirthday.date());
-        console.log("农历日期:", now.year(), recordedBirthday.month()+1, recordedBirthday.date());
-        console.log("转换后日期:", cYear, cMonth, cDay); // 这里如果出现 比如说农历是12月29号啥的 会跨年，怎么处理呢
+        // console.log("农历日期:", now.year(), recordedBirthday.month()+1, recordedBirthday.date());
+        // console.log("转换后日期:", cYear, cMonth, cDay); // 这里如果出现 比如说农历是12月29号啥的 会跨年，怎么处理呢
         var solar_str = ""+cYear+"-"+cMonth+"-"+cDay;
-        var solar_date = dayjs(cYear+"-"+cMonth+"-"+cDay,"YYYY-M-D")
-        console.log("看看")
-        console.log(solar_str)
-    }
+        solar_date = dayjs(solar_str,"YYYY-M-D")
+        // console.log("看看")
+        // console.log(solar_date)
 
-
-    // 如果用户输入的日期是 "YYYY-MM-DD" 格式，比如 "2000-07-12"
-    // 需要把这里的 "YYYY" 统一转换到当前的年份进行计算
-    if(birthday_calculate.year() != now.year())
-        birthday_calculate = birthday_calculate.year(now.year());
+        if(solar_date.isBefore(now, 'day')){
+            // 如果生日在今天之前，那么还有一年；
+            const nextyear_str = lunar_trans.lunar2solar(cur_lunar_year+1,recordedBirthday.month()+1, recordedBirthday.date()).date;
+            const nextBirthday = dayjs(nextyear_str,"YYYY-M-D");
+            diffInDays = nextBirthday.startOf('day').diff(now.startOf('day'), 'day');
+            // todo
+        } else {
+            // 如果生日在今天之后，那么离生日还有几天
+            diffInDays = solar_date.startOf('day').diff(now.startOf('day'), 'day');
+        }
+    } 
+    // 公历的情况 calendar == "solar"
+    else {
+        var birthday_calculate = recordedBirthday.clone(); // 复制一份，用于计算天数差
         
-    let diffInDays; // 距离朋友生日的天数
-    if(birthday_calculate.isBefore(now, 'day')){
-        // 如果生日在今天之前，那么离生日还有一年
-        const nextBirthday = birthday_calculate.add(1, 'year');
-        diffInDays = nextBirthday.startOf('day').diff(now.startOf('day'), 'day');
-    } else {
-        // 如果生日在今天之后，那么离生日还有几天
-        diffInDays = birthday_calculate.startOf('day').diff(now.startOf('day'), 'day');
+        // 如果用户输入的日期是 "YYYY-MM-DD" 格式，比如 "2000-07-12"
+        // 需要把这里的 "YYYY" 统一转换到当前的年份进行计算
+        if(birthday_calculate.year() != now.year())
+        birthday_calculate = birthday_calculate.year(now.year());
+            
+        if(birthday_calculate.isBefore(now, 'day')){
+            // 如果生日在今天之前，那么离生日还有一年
+            const nextBirthday = birthday_calculate.add(1, 'year');
+            diffInDays = nextBirthday.startOf('day').diff(now.startOf('day'), 'day');
+        } else {
+            // 如果生日在今天之后，那么离生日还有几天
+            diffInDays = birthday_calculate.startOf('day').diff(now.startOf('day'), 'day');
+        }
     }
-    console.log(birthday_calculate.year(),diffInDays);
-    console.log("advancedDay:",advancedDay);
+
+    // 公共逻辑提出来
+    
     for(let advanced of advancedDay){
         if(advanced == diffInDays){
-            console.log("发送邮件")
+            console.log("存入 reminds 数组")
+            // 传入参数，发送邮件
+            // 0. 这里不太对，应该不是直接发送邮件，而是把这些对象封装到一个数组里，统一发邮件
+            // 1. 用户信息，封装成对象传过去吧（名字、公历生日、农历生日(如果有)、
+            //                              几岁了(如果有)、朋友的邮箱(考虑删掉)、还差几天生日）
+            let frind_info = {
+                name: name,
+                solar_birthday: recordedBirthday.format("MM-DD"), // 转化成 MM-DD 格式的字符串哈
+                lunar_birthday: solar_date == undefined ? null : solar_date.format("MM-DD"),
+                age: recordedBirthday.year() >= now.year() ? null : now.year() - recordedBirthday.year(),
+                email: email == "" ? null : email,
+                diff: diffInDays
+            }
+            reminds.push(frind_info);
+            console.log("reminds");
+            console.log(reminds);
             break;
         }
     }
 }
-console.log("现在:","2023-01-10");
-console.log("测试农历年:",lunar_trans.solar2lunar("2023","01","10").lunarDate);
-console.log("朋友1生日 农历12月25")
-let test = lunar_trans.lunar2solar("2022","12","25")
-console.log("朋友1生日 公历:",test.date);
-console.log("朋友2生日 农历1月8")
-let test2 = lunar_trans.lunar2solar("2023","01","08")
-console.log("朋友2生日 公历:",test2.date);
+
+// 发送邮件, reminds 作为参数～（应该还要拼接上 holidayInfo）
+console.log("这里发送邮件")
+sendEmail(reminds);
+
+// console.log("现在:","2023-01-10");
+// console.log("测试农历年:",lunar_trans.solar2lunar("2023","01","10").lunarDate);
+// console.log(typeof(lunar_trans.solar2lunar("2023","01","10").lunarDate))
+// console.log("朋友1生日 农历12月25")
+// let test = lunar_trans.lunar2solar("2022","12","25")
+// console.log("朋友1生日 公历:",test.date);
+// console.log("朋友2生日 农历1月8")
+// let test2 = lunar_trans.lunar2solar("2023","01","08")
+// console.log("朋友2生日 公历:",test2.date);
 
 
 // 现在：2023-01-10
--- sendEmail.js
+++ sendEmail.js
@@ -1,21 +1,6 @@
 const { error, info } = require('console');
 const nodemailer = require('nodemailer');
 
-const htmlStr = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-  <body>
-    <h1>大标题test</h1>
-    <p>p标签test</p>
-  </body>
-</html>
-`
-
 const get_env = (key) => {
     let env_val = process.env[key]; // 获取对应参数的环境变量
     if(env_val == undefined){
@@ -48,18 +33,47 @@ const receiver_arr = (str) => {
     }
 }
 
-const mailOptions = {
-    from: get_env("SENDER"),
-    // to: 'RuizhengWu@outlook.com', 
-    to: receiver_arr(get_env("RECEIVERS")), // 这里的to 既可以是string，也可以是string 数组
-    subject: '一封测试邮件',
-    html: htmlStr
-};
+const sendEmail = (reminds) => {
+    // 生日提醒 和 节日提醒 要做下判断，三元表达式之类的
+    var htmlStr = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </head>
+        <body>
+            <h1>生日提醒</h1>
+            <ul>
+                ${reminds.map(
+                    remind =>
+                    `<li>姓名：${remind.name}，还有 ${remind.diff} 天就要过生日了！
+                        公历生日：${remind.solar_birthday}，
+                        阳历生日：${remind.lunar_birthday == null ? "未记录" : ""},
+                        过生日时的年龄是：${remind.age == null ? "未记录" : ""},
+                    </li>`
+                ).join('')}
+            </ul>
+            <h1>节日提醒</h1>
+        </body>
+        </html>
+    `
+    const mailOptions = {
+        from: get_env("SENDER"),
+        // to: 'RuizhengWu@outlook.com', 
+        to: receiver_arr(get_env("RECEIVERS")), // 这里的to 既可以是string，也可以是string 数组
+        subject: '生日提醒Bot',
+        html: htmlStr
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) =>{
+        if(error){
+            console.log(error);
+        } else {
+            console.log('发送成功: ' + info.response);
+        }
+    })
+}
 
-transporter.sendMail(mailOptions, (error, info) =>{
-    if(error){
-        console.log(error);
-    } else {
-        console.log('发送成功: ' + info.response);
-    }
-})
+module.exports = sendEmail;
